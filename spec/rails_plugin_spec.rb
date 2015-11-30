8bb2e72dde62218e0f353aeac37668313f7d1fd8
Pavel <pavel.evst@gmail.com>
2015-11-30 22:40:13 +0700

    Test against different versions of rails

--- spec/rails_plugin_spec.rb
+++ spec/rails_plugin_spec.rb
@@ -6,9 +6,11 @@ require 'socket'
 describe "Rails plugin", vcr: false do
   include Capybara::DSL
 
-  RAILS_VER = "4.1.9"
+  MAIN_RAILS_VER = "4.1.9"
   APP_DIR = "plugin_test"
   PLUGIN_DIR = File.expand_path("..", File.dirname(__FILE__))
+
+  RAILS_VERSIONS = ["4.0.13", "4.1.14", "4.2.5"]
 
   before :all do
     FileUtils.mkdir_p("#{PLUGIN_DIR}/tmp")
@@ -44,37 +46,37 @@ describe "Rails plugin", vcr: false do
     return stdout_str
   end
 
-  def install_rails_in_tmp
+  def install_rails_in_tmp(rails_version = MAIN_RAILS_VER)
     @rails_dir = Dir.mktmpdir()
     @app_abs_path = "#{@rails_dir}/#{APP_DIR}"
 
     Dir.chdir(@rails_dir) do
       Bundler.with_clean_env do
         ENV["RAILS_ENV"] = "development"
-        find_or_install_rails
-        generate_rails_app
+        find_or_install_rails(rails_version)
+        generate_rails_app(rails_version)
         generate_plugin_things
       end
     end
   end
 
-  def find_or_install_rails
+  def find_or_install_rails(rails_version)
     response = run_cmd('gem list \^rails\$ -q')
-    if response =~ /[^\d]#{RAILS_VER}[^\d]/
+    if response =~ /[^\d]#{rails_version}[^\d]/
       return true
     else
-      run_cmd("gem install rails -v #{RAILS_VER} --no-ri --no-rdoc")
+      run_cmd("gem install rails -v #{rails_version} --no-ri --no-rdoc")
     end
   end
 
-  def generate_rails_app
-    gen = "rails _#{RAILS_VER}_ new #{APP_DIR} -B -G --skip-spring -d sqlite3 --skip-turbolinks --skip-test-unit --no-rc"
+  def generate_rails_app(rails_version)
+    gen = "rails _#{rails_version}_ new #{APP_DIR} -B -G --skip-spring -d sqlite3 --skip-turbolinks --skip-test-unit --no-rc"
     run_cmd(gen)
 
     gemfile_content = "
       source 'https://rubygems.org'
 
-      gem 'rails', '#{RAILS_VER}'
+      gem 'rails', '#{rails_version}'
       gem 'sqlite3'
       gem 'turbolinks'
       gem 'jquery-rails'
@@ -150,63 +152,65 @@ development:
     FileUtils.remove_entry_secure(@rails_dir) if @rails_dir
   end
 
-  it "should tests plugin" do
-    # PREPARE APP
-    install_rails_in_tmp
-    run_rails_app
+  RAILS_VERSIONS.each do |rails_version|
+    it "should tests plugin (Rails #{rails_version})" do
+      # PREPARE APP
+      install_rails_in_tmp(rails_version)
+      run_rails_app
 
-    # CREATE PAYMENT 1
-    visit "/payments/new"
+      # CREATE PAYMENT 1
+      visit "/payments/new"
 
-    click_button "Pay via VT-Direct"
+      click_button "Pay via VT-Direct"
 
-    # Waiting for get token request in javascript...
-    Timeout.timeout(30.seconds) do
-      loop do
-        break if current_path != "/payments/new"
-        sleep 0.1
+      # Waiting for get token request in javascript...
+      Timeout.timeout(30.seconds) do
+        loop do
+          break if current_path != "/payments/new"
+          sleep 0.1
+        end
       end
-    end
 
-    Timeout.timeout(10.seconds) do
-      loop do
-        break if page.body =~ /<body>/
-        sleep 0.1
+      Timeout.timeout(10.seconds) do
+        loop do
+          break if page.body =~ /<body>/
+          sleep 0.1
+        end
       end
+
+      if page.body =~ /too many transactions/
+        page.should have_content("Merchant has sent too many transactions to the same card number")
+      else
+        page.should have_content("Success, Credit Card transaction is successful")
+      end
+
+      created_order_id = ActiveSupport::JSON.decode(find("pre").text)["order_id"]
+      #Capybara::Screenshot.screenshot_and_open_image
+
+      # CREATE PAYMENT 2
+      visit "/payments/new"
+      click_link "Pay via VT-Web"
+
+      page.should have_content("Payment is securely processed by Veritrans")
+      #Capybara::Screenshot.screenshot_and_open_image
+
+      # TEST CALLBACK FOR WRONG DATA
+      stub_const("CONFIG", {})
+      result1 = capture_stdout do
+        Veritrans::CLI.test_webhook(["#{Capybara.app_host}/payments/receive_webhook"])
+      end
+
+      result1.should =~ /status: 404/
+
+      # TEST CALLBACK FOR CORRECT DATA
+      stub_const("CONFIG", {order: created_order_id, config_path: "#{@app_abs_path}/config/veritrans.yml"})
+      result2 = capture_stdout do
+        Veritrans::CLI.test_webhook(["#{Capybara.app_host}/payments/receive_webhook"])
+      end
+
+      result2.should =~ /status: 200/
+      result2.should =~ /body: ok/
     end
-
-    if page.body =~ /too many transactions/
-      page.should have_content("Merchant has sent too many transactions to the same card number")
-    else
-      page.should have_content("Success, Credit Card transaction is successful")
-    end
-
-    created_order_id = ActiveSupport::JSON.decode(find("pre").text)["order_id"]
-    #Capybara::Screenshot.screenshot_and_open_image
-
-    # CREATE PAYMENT 2
-    visit "/payments/new"
-    click_link "Pay via VT-Web"
-
-    page.should have_content("Payment is securely processed by Veritrans")
-    #Capybara::Screenshot.screenshot_and_open_image
-
-    # TEST CALLBACK FOR WRONG DATA
-    stub_const("CONFIG", {})
-    result1 = capture_stdout do
-      Veritrans::CLI.test_webhook(["#{Capybara.app_host}/payments/receive_webhook"])
-    end
-
-    result1.should =~ /status: 404/
-
-    # TEST CALLBACK FOR CORRECT DATA
-    stub_const("CONFIG", {order: created_order_id, config_path: "#{@app_abs_path}/config/veritrans.yml"})
-    result2 = capture_stdout do
-      Veritrans::CLI.test_webhook(["#{Capybara.app_host}/payments/receive_webhook"])
-    end
-
-    result2.should =~ /status: 200/
-    result2.should =~ /body: ok/
   end
 
   it "should print message if running in staging" do
