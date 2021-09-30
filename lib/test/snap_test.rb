71dda45daa746f2a110f7ceec4096a0a3eaef5e6
Andrenzo17 <83454487+Andrenzo17@users.noreply.github.com>
2021-09-30 16:22:07 +0700

    add unit test with invalid server key

--- lib/test/snap_test.rb
+++ lib/test/snap_test.rb
@@ -16,7 +16,7 @@ class TestVeritrans < Minitest::Test
   def test_create_widget_token
     result = @mt_test.create_widget_token(
       transaction_details: {
-        order_id: "THE-ITEM-ORDER-#{rand(1..10000)}",
+        order_id: "ruby-lib-test-#{Time.now.to_i}",
         gross_amount: 200000
       },
       "credit_card": {
@@ -24,6 +24,28 @@ class TestVeritrans < Minitest::Test
       }
     )
     assert_equal 201, result.status_code
+  end
+
+  def test_snap_invalid_serverkey
+    @mt_test_invalid_key = Veritrans.new(
+      server_key: "invalid server key",
+      client_key: "invalid client key",
+      api_host: "https://api.sandbox.midtrans.com",
+      logger: Logger.new(STDOUT),
+      file_logger: Logger.new(STDOUT)
+    )
+
+    result = @mt_test_invalid_key.create_widget_token(
+      transaction_details: {
+        order_id: "ruby-lib-test-#{Time.now.to_i}",
+        gross_amount: 200000
+      },
+      "credit_card": {
+        "secure": true
+      }
+    )
+    assert_equal 401, result.status_code
+    assert_equal ["Access denied due to unauthorized transaction, please check client or server key", "Visit https://snap-docs.midtrans.com/#request-headers for more details"], result.error_messages
   end
 
   end
